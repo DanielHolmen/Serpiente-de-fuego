3501f74b2c146310f86b33b2fe43b7aed24ee650
einar05 <einarskoien@icloud.com>
2024-04-03 19:09:35 +0200

    endret størrelse på spillvinduet

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import random
 
 # Konstanter
 WIDTH = 1100  # Bredden til vinduet
-HEIGHT = 700 # Høyden til vinduet
+HEIGHT = 650 # Høyden til vinduet
 
 # Størrelsen til vinduet
 SIZE = (WIDTH, HEIGHT)
